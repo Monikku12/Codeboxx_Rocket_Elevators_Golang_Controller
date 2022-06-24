5c9efd6b27e212e39647dae0b7841d7b4bbb8b8a
Monikku12 <monique.paymentboulanger@gmail.com>
2022-06-24 23:11:37 +0900

    FloorRequestButton : done

--- floorRequestButtons.go
+++ floorRequestButtons.go
@@ -2,9 +2,15 @@ package main
 
 //FloorRequestButton is a button on the pannel at the lobby to request any floor
 type FloorRequestButton struct {
-	
+	ID int
+	status string
+	floorRequestButtonID int
+	buttonFloor int
+	direction string
 }
 
-func NewFloorRequestButton(_floor int, _direction string) *FloorRequestButton {
+func NewFloorRequestButton(ID int, status string, _buttonFloor int, _direction string) *FloorRequestButton {
+	floorRequestButton := &FloorRequestButton{ID: ID, status: status, buttonFloor: _buttonFloor, direction: _direction}
 
+	return floorRequestButton
 }
