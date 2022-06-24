c600ea11a2878fba27d83e3a370eb6e9f3773c6c
Monikku12 <monique.paymentboulanger@gmail.com>
2022-06-25 05:25:40 +0900

    OperateDoors : Done

--- column.go
+++ column.go
@@ -1,12 +1,20 @@
 package main
 
-// type Column struct {
+type Column struct {
+	ID int
+	status string
+	amountOfFloors int
+	servedFloorsList []int
+	amountOfElevators int
+	isBasement bool
+	elevatorsList []Elevator
+	callButtonsList []CallButton
 
-// }
+}
 
-// func NewColumn(_id, _amountOfElevators int, _servedFloors []int, _isBasement bool) *Column {
+func NewColumn(_id, _amountOfElevators int, _servedFloors []int, _isBasement bool) *Column {
 	
-// }
+}
 
 // //Simulate when a user press a button on a floor to go back to the first floor
 // func (c *Column) requestElevator(_requestedFloor int, _direction string) *Elevator {
--- elevator.go
+++ elevator.go
@@ -1,24 +1,82 @@
 package main
 
+import (
+	"fmt"
+)
+
 type Elevator struct {
 	ID int
 	status string
 	amountOfFloors int
 	currentFloor int
 	direction string
-	door Door
 	overweight bool
 	obstruction bool
+	door Door
 	completedRequestsList []int
 	floorRequestsList []int
 }
 
 func NewElevator(ID int, _status string, _amountOfFloors int, _currentFloor int) *Elevator {
-	elevator := &Elevator{ID: ID, status: "idle", amountOfFloors: _amountOfFloors, currentFloor: _currentFloor}
+	
+	door := Door{ID: ID, status: "closed"}
 
+	elevator := &Elevator{ID: ID, status: "idle", amountOfFloors: _amountOfFloors, currentFloor: _currentFloor, Door: door}
+	
 	return elevator
 }
 
-// func (e *Elevator) move() {
+func (e *Elevator) move() {
 	
-// }
+	for true{
+		if len(e.floorRequestsList) != 0 {
+		var destination = e.floorRequestsList[0]
+		e.status = "moving"
+			if e.currentFloor < destination {
+				e.direction = "up"
+				e.sortFloorList()
+				if e.currentFloor < destination {
+					e.currentFloor++
+					var screenDisplay = e.currentFloor
+					}
+			}
+		}
+	}
+}
+
+func (e *Elevator) operateDoors() {
+
+	e.door.status = "opened"
+	fmt.Println("Wait 5 seconds")
+	if e.overweight == false {
+		e.door.status = "closing"
+		if e.obstruction == false {
+			e.door.status = "closed"
+			break
+		}
+		if e.operateDoors(){
+			break
+		}
+		break
+	}
+	if e.overweight == true {
+		fmt.Println("Activate overweight alarm")
+	}
+	e.operateDoors()
+	break
+}
+
+
+
+
+
+
+
+
+
+
+
+func (e *Elevator) addNewRequest() {
+
+	// if e.floorRequestsList.
+}
